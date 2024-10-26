967a7078f7800fd24c87fe2cf7a24d9b4f9b4348
jazevedo <jazevedo@student.42.rio>
2024-10-26 17:08:08 -0300

    Add: start_map().

--- macros.py
+++ macros.py
@@ -1,11 +1,8 @@
-# ----------| IMAGES |---------- #
-WALL = pygame.image.load('wall.png')
-WAY = pygame.image.load('way.png')
-COIN = pygame.image.load('coin.png')
-EXIT = pygame.image.load('exit.png')
-PLAYER = pygame.image.load('player.png')
+from utils import err
+
 
 # ----------| ERRORS |---------- #
 ERR_FILENAME = "Erro! '{filename} é invalido. A extensão deve ser '.map'."
 ERR_NOTFOUND = "Erro! Mapa '{filename}' não encontrado."
 ERR_MAP = "Erro! Mapa inválido."
+ERR_IMAGE = "Erro! Imagem '{image}' é inválida."
--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 from verify_map import get_map, invalid_map
+import pygame
+
+try:
+    WALL = pygame.image.load('wall.png')
+    WAY = pygame.image.load('way.png')
+    COIN = pygame.image.load('coin.png')
+    EXIT = pygame.image.load('exit.png')
+    PLAYER = pygame.image.load('player.png')
+except pygame.error as error:
+    err(ERR_IMAGE.format(image=error))
+
+IMAGE = 16
+
+def start_map(maps):
+    width = len(maps[0]) * IMAGE
+    height = len(maps) * IMAGE
+
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Game of Coins")
+
+    for y, line in enumerate(maps):
+        for x, i in enumerate(line):
+            if (i == '1'):
+                screen.blit(WALL, (x * IMAGE, y * IMAGE))
+            elif (i == '0'):
+                screen.blit(WAY, (x * IMAGE, y * IMAGE))
+            elif (i == 'C'):
+                screen.blit(COIN, (x * IMAGE, y * IMAGE))
+            elif (i == 'E'):
+                screen.blit(EXIT, (x * IMAGE, y * IMAGE))
+            elif (i == 'P'):
+                screen.blit(PLAYER, (x * IMAGE, y * IMAGE))
+
+    pygames.display.flip()
+
+
+# def map_loop(maps):
+
 
 def main():
-    
+   pygame.init()
+
     maps = get_map()
     if (maps is None):
         return
     if (invalid_map(maps)):
         return
 
-    # start_map(maps)
-    # map_loop()
+    start_map(maps)
+    # map_loop(maps)
