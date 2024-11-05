19e6060d94cf31b488ee32ada215e8ff4b668141
jazevedo <jazevedo@student.42.rio>
2024-11-05 18:01:43 -0300

    Add: Enemies.

--- start_map.py
+++ start_map.py
@@ -1,6 +1,7 @@
 from macros import ERR_IMAGE
 from utils import err, count_char
 import pygame
+import random
 
 try:
     WALL = pygame.image.load('Textures/wall.png')
@@ -8,6 +9,7 @@ try:
     COIN = pygame.image.load('Textures/coin.png')
     EXIT = pygame.image.load('Textures/exit.png')
     PLAYER = pygame.image.load('Textures/player.png')
+    ENEMY = pygame.image.load('Textures/enemy.png')
 except pygame.error as error:
     err(ERR_IMAGE.format(image=error))
 
@@ -20,7 +22,7 @@ def start_map(maps):
     height = len(maps) * IMAGE
 
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Game of Coins")
+    pygame.display.set_caption("| Ruined Castle |")
 
     for y, line in enumerate(maps):
         for x, i in enumerate(line):
@@ -34,7 +36,8 @@ def start_map(maps):
                 screen.blit(EXIT, (x * IMAGE, y * IMAGE))
             elif (i == 'P'):
                 screen.blit(PLAYER, (x * IMAGE, y * IMAGE))
-
+            elif (i == 'G'):
+                screen.blit(ENEMY, (x * IMAGE, y * IMAGE))
     pygame.display.flip()
     return screen
 
@@ -50,13 +53,79 @@ def find_player(maps):
 def update_map(screen, c, x, y):
     if (c == '0'):
         screen.blit(WAY, (x * IMAGE, y * IMAGE))
+    elif (c == '1'):
+        screen.blit(WALL, (x * IMAGE, y * IMAGE))
     elif (c == 'C'):
         screen.blit(COIN, (x * IMAGE, y * IMAGE))
     elif (c == 'E'):
         screen.blit(EXIT, (x * IMAGE, y * IMAGE))
     elif (c == 'P'):
         screen.blit(PLAYER, (x * IMAGE, y * IMAGE))
+    elif (c == 'G'):
+        screen.blit(ENEMY, (x * IMAGE, y * IMAGE))
     pygame.display.update(pygame.Rect(x * IMAGE, y * IMAGE, IMAGE, IMAGE))
+
+
+def enemy_around(maps, x, y):
+    if (maps[y - 1][x] == 'G' or maps[y + 1][x] == 'G'):
+        return True
+    elif (maps[y][x - 1] == 'G' or maps[y][x + 1] == 'G'):
+        return True
+    elif (maps[y - 1][x - 1] == 'G' or maps[y + 1][x + 1] == 'G'):
+        return True
+    elif (maps[y - 1][x + 1] == 'G' or maps[y + 1][x - 1] == 'G'):
+        return True
+    return False
+
+
+def get_around(maps, x, y):
+    around = []
+    if (maps[y - 1][x] == 'G'):
+        around.append((x, y - 1))
+    if (maps[y + 1][x] == 'G'):
+        around.append((x, y + 1))
+    if (maps[y][x - 1] == 'G'):
+        around.append((x - 1, y))
+    if (maps[y][x + 1] == 'G'):
+        around.append((x + 1, y))
+    if (maps[y - 1][x - 1] == 'G'):
+        around.append((x - 1, y - 1))
+    if (maps[y + 1][x + 1] == 'G'):
+        around.append((x + 1, y + 1))
+    if (maps[y - 1][x + 1] == 'G'):
+        around.append((x + 1, y - 1))
+    if (maps[y + 1][x - 1] == 'G'):
+        around.append((x - 1, y + 1))
+    return around
+
+
+def move_enemies(maps, screen):
+    for y, line in enumerate(maps):
+        for x, i in enumerate(line):
+            if (i == 'G'):
+                newx, newy = x, y
+                direction = random.choice([0, 1, 2, 3])
+                tempx, tempy = newx, newy
+                if (direction == 0):
+                    tempy -= 1
+                elif (direction == 1):
+                    tempy += 1
+                elif (direction == 2):
+                    tempx -= 1
+                elif (direction == 3):
+                    tempx += 1
+
+                if (0 <= tempy < len(maps) and 0 <= tempx < len(maps[0])):
+                    if (maps[tempy][tempx] == '0'):
+                        newx, newy = tempx, tempy
+                    elif (maps[tempy][tempx] == 'P'):
+                        return None
+
+                maps[y][x] = '0'
+                update_map(screen, '0', x, y)
+                maps[newy][newx] = 'G'
+                update_map(screen, 'G', newx, newy)
+    return maps
 
 
 def map_loop(screen, maps):
@@ -70,7 +139,10 @@ def map_loop(screen, maps):
                 running = False
             elif (event.type == pygame.KEYDOWN):
                 newx, newy = player
-                if (event.key == pygame.K_UP):
+                if (event.key == pygame.K_ESCAPE):
+                    running = False
+                    break
+                elif (event.key == pygame.K_UP):
                     newy -= 1
                 elif (event.key == pygame.K_DOWN):
                     newy += 1
@@ -78,6 +150,13 @@ def map_loop(screen, maps):
                     newx -= 1
                 elif (event.key == pygame.K_RIGHT):
                     newx += 1
+                elif (event.key == pygame.K_SPACE and enemy_around(maps, *player)):
+                    enemies_around = get_around(maps, *player)
+                    if (enemies_around):
+                        for enemy in enemies_around:
+                            update_map(screen, '0', *enemy)
+                            maps[enemy[1]][enemy[0]] = '0'
+                        continue
 
                 if (maps[newy][newx] == 'E'):
                     if (coins < 1):
@@ -86,7 +165,18 @@ def map_loop(screen, maps):
                         break
                     else:
                         continue
+
+                if (maps[newy][newx] == 'G'):
+                    running = False
+                    print("Game Over!")
+                    break
+
                 if (maps[newy][newx] != '1'):
+                    maps = move_enemies(maps, screen)
+                    if (maps is None):
+                        running = False
+                        print("Game Over!")
+                        break
                     if (maps[newy][newx] == 'C'):
                         maps[newy][newx] = '0'
                         coins -= 1
--- verify_map.py
+++ verify_map.py
@@ -30,30 +30,39 @@ def isnt_rectangular(maps):
 
 def invincible_map(maps):
     player_pos = None
-    exit_pos = None
     coin_pos = set()
+    exit_pos = None
 
     for y, line in enumerate(maps):
         for x, i in enumerate(line):
-            if (i == 'P'):
+            if i == 'P':
                 player_pos = (y, x)
-            elif (i == 'C'):
+            elif i == 'C':
                 coin_pos.add((y, x))
+            elif i == 'E':
+                exit_pos = (y, x)
 
-    def floodfill(y, x, visited):
-        if ((y, x) in visited or maps[y][x] in ('1', 'E')):
+    def floodfill(y, x, visited, ignore_exit=True):
+        if (y, x) in visited or maps[y][x] == '1' or (ignore_exit and maps[y][x] == 'E'):
             return
 
         visited.add((y, x))
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for directy, directx in directions:
-            newy, newx = y + directy, x + directx
-            if ((0 <= newy < len(maps)) and (0 <= newx < len(maps[0]))):
-                floodfill(newy, newx, visited)
+        for dy, dx in directions:
+            ny, nx = y + dy, x + dx
+            if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]):
+                floodfill(ny, nx, visited, ignore_exit)
 
     visited = set()
-    floodfill(player_pos[0], player_pos[1], visited)
-    if (not coin_pos.issubset(visited)):
+    floodfill(player_pos[0], player_pos[1], visited, ignore_exit=True)
+
+    if not coin_pos.issubset(visited):
+        return True
+
+    visited.clear()
+    floodfill(player_pos[0], player_pos[1], visited, ignore_exit=False)
+
+    if exit_pos not in visited:
         return True
 
     return False
@@ -62,8 +71,9 @@ def invincible_map(maps):
 def invalid_map(maps):
     for line in maps:
         for i in line:
-            if (i not in ['0', '1', 'C', 'E', 'P']):
+            if (i not in ['0', '1', 'C', 'E', 'P', 'G']):
                 err(ERR_MAP)
+                err("Caractere inválido.")
                 return True
 
     player = count_char(maps, 'P') != 1
@@ -71,18 +81,22 @@ def invalid_map(maps):
     coin = count_char(maps, 'C') < 1
     if (player or exitt or coin or isnt_rectangular(maps)):
         err(ERR_MAP)
+        err("O mapa não é retangular || player != 1, exit != 1, coin < 1.")
         return True
 
     if ((not all(i == '1' for i in maps[0])) or (not all(i == '1' for i in maps[-1]))):
         err(ERR_MAP)
+        err("As bordas do mapa devem ser paredes.")
         return True
     for line in maps:
         if (line[0] != '1' or line[-1] != '1'):
             err(ERR_MAP)
+            err("As bordas do mapa devem ser paredes.")
             return True
 
     if (invincible_map(maps)):
         err(ERR_MAP)
+        err("O mapa é invencível.")
         return True
 
     return False
